e416a944a4d381f3847d4a08ca16fa88e33e8a50
asha330 <ashakumar20004@gmail.com>
2024-10-07 19:44:33 +0530

    Update m1.js

--- m1.js
+++ m1.js
@@ -6,6 +6,7 @@ const loginContainer = document.getElementById('login-container');
 const postContainer = document.getElementById('post-container');
 
 let posts = JSON.parse(localStorage.getItem('posts')) || [];
+let currentUser = null; // Store the current logged-in user
 
 // Function to display posts
 function displayPosts() {
@@ -14,111 +15,9 @@ function displayPosts() {
         const postElement = document.createElement('div');
         postElement.className = 'post';
         postElement.innerHTML = `
+            <strong>${post.username}</strong> <br>
             <p>${post.content}</p>
-            <p><small>${post.date}</small></p>
+            <p><small>${timeSince(new Date(post.date))} ago</small></p>
             <p>👍 Likes: ${post.likes} | 👎 Dislikes: ${post.dislikes}</p>
             <button class="like" onclick="likePost(${postIndex})">👍 Like</button>
-            <button class="dislike" onclick="dislikePost(${postIndex})">👎 Dislike</button>
-            <input type="text" class="comment-input" id="comment-input-${postIndex}" placeholder="Add a comment with emojis">
-            <button onclick="addComment(${postIndex})">💬 Comment</button>
-            <div>
-                <strong>Comments:</strong>
-                <ul>
-                    ${post.comments.map((comment, commentIndex) => `
-                        <li>
-                            ${comment.text} 
-                            <br> 👍 ${comment.likes} 👎 ${comment.dislikes}
-                            <button onclick="likeComment(${postIndex}, ${commentIndex})">👍 Like</button>
-                            <button onclick="dislikeComment(${postIndex}, ${commentIndex})">👎 Dislike</button>
-                        </li>
-                    `).join('')}
-                </ul>
-            </div>
-        `;
-        postsContainer.appendChild(postElement);
-    });
-}
-
-// Handle login
-loginForm.addEventListener('submit', (event) => {
-    event.preventDefault();
-    const username = document.getElementById('username').value;
-
-    if (username) {
-        alert(`Login successful! Welcome, ${username}!`);
-        loginContainer.style.display = 'none';
-        postContainer.style.display = 'block'; // Show post container
-    } else {
-        alert('Please enter a username.');
-    }
-});
-
-// Function to handle post submission
-postForm.addEventListener('submit', (event) => {
-    event.preventDefault();
-    const content = postContent.value;
-    const newPost = {
-        content: content,
-        likes: 0,
-        dislikes: 0,
-        comments: [],
-        date: new Date().toLocaleString() // Store current date and time
-    };
-    posts.push(newPost);
-    postContent.value = '';
-    savePosts(); // Save posts in localStorage
-    displayPosts();
-});
-
-// Function to like a post
-function likePost(index) {
-    posts[index].likes++;
-    savePosts();
-    displayPosts();
-}
-
-// Function to dislike a post
-function dislikePost(index) {
-    posts[index].dislikes++;
-    savePosts();
-    displayPosts();
-}
-
-// Function to add a comment with likes and dislikes
-function addComment(index) {
-    const commentInput = document.getElementById(`comment-input-${index}`);
-    const comment = commentInput.value;
-    if (comment) {
-        const newComment = {
-            text: comment + ' 😊',
-            likes: 0,
-            dislikes: 0
-        };
-        posts[index].comments.push(newComment);
-        commentInput.value = '';
-        savePosts();
-        displayPosts();
-    }
-}
-
-// Function to like a comment
-function likeComment(postIndex, commentIndex) {
-    posts[postIndex].comments[commentIndex].likes++;
-    savePosts();
-    displayPosts();
-}
-
-// Function to dislike a comment
-function dislikeComment(postIndex, commentIndex) {
-    posts[postIndex].comments[commentIndex].dislikes++;
-    savePosts();
-    displayPosts();
-}
-
-// Save posts to localStorage
-function savePosts() {
-    localStorage.setItem('posts', JSON.stringify(posts));
-}
-
-// Initial display of posts
-displayPosts();
+            <⬤
