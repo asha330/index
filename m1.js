0c4ccbe1e012ad2d685bc647436459d7a514d3f0
asha330 <ashakumar20004@gmail.com>
2024-10-07 14:13:18 +0530

    Update m1.js

--- m1.js
+++ m1.js
@@ -5,7 +5,7 @@ const loginForm = document.getElementById('login-form');
 const loginContainer = document.getElementById('login-container');
 const postContainer = document.getElementById('post-container');
 
-let posts = [];
+let posts = JSON.parse(localStorage.getItem('posts')) || [];
 
 // Function to display posts
 function displayPosts() {
@@ -15,12 +15,12 @@ function displayPosts() {
         postElement.className = 'post';
         postElement.innerHTML = `
             <p>${post.content}</p>
-            <p><small>${post.date.toLocaleString()}</small></p>
-            <p>Likes: ${post.likes} | Dislikes: ${post.dislikes}</p>
+            <p><small>${post.date}</small></p>
+            <p>👍 Likes: ${post.likes} | 👎 Dislikes: ${post.dislikes}</p>
             <button class="like" onclick="likePost(${index})">👍 Like</button>
             <button class="dislike" onclick="dislikePost(${index})">👎 Dislike</button>
-            <input type="text" class="comment-input" id="comment-input-${index}" placeholder="Add a comment">
-            <button onclick="addComment(${index})">Comment</button>
+            <input type="text" class="comment-input" id="comment-input-${index}" placeholder="Add a comment with emojis">
+            <button onclick="addComment(${index})">💬 Comment</button>
             <div>
                 <strong>Comments:</strong>
                 <ul>${post.comments.map(comment => `<li>${comment}</li>`).join('')}</ul>
@@ -54,22 +54,25 @@ postForm.addEventListener('submit', (event) => {
         likes: 0,
         dislikes: 0,
         comments: [],
-        date: new Date() // Store current date and time
+        date: new Date().toLocaleString() // Store current date and time
     };
     posts.push(newPost);
     postContent.value = '';
+    savePosts(); // Save posts in localStorage
     displayPosts();
 });
 
 // Function to like a post
 function likePost(index) {
     posts[index].likes++;
+    savePosts();
     displayPosts();
 }
 
 // Function to dislike a post
 function dislikePost(index) {
     posts[index].dislikes++;
+    savePosts();
     displayPosts();
 }
 
@@ -78,10 +81,16 @@ function addComment(index) {
     const commentInput = document.getElementById(`comment-input-${index}`);
     const comment = commentInput.value;
     if (comment) {
-        posts[index].comments.push(comment);
+        posts[index].comments.push(comment + ' 😊');
         commentInput.value = '';
+        savePosts();
         displayPosts();
     }
+}
+
+// Save posts to localStorage
+function savePosts() {
+    localStorage.setItem('posts', JSON.stringify(posts));
 }
 
 // Initial display of posts
